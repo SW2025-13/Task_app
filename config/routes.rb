68f8954aa2e20aa52100a2cd6d864b4167b7699b
takano takuya <2311140058h@ed.fuk.kindai.ac.jp>
2025-11-12 08:54:18 +0000

    create_contoroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  root "tasks#index"
+
+  get  "signup", to: "users#new"
+  post "signup", to: "users#create"
+
+  get    "login",  to: "sessions#new"
+  post   "login",  to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+
+  resources :tasks do
+    member do
+      post "toggle_done"
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
