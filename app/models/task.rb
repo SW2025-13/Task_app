dd7d37099198db0a85b456507bd3f10fcd73069b
Naofumi Shiota <2311140037x@ed.fuk.kindai.ac.jp>
2025-11-19 06:33:30 +0000

    塩田 作業同期

---
+++ app/models/task.rb
@@ -0,0 +1,7 @@
+class Task < ApplicationRecord
+  belongs_to :user
+
+  validates :title, presence: true
+  validates :priority, inclusion: { in: 1..5 }
+  validates :effort, inclusion: { in: 1..5 }
+end
