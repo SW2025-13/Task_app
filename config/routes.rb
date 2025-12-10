2a3ed63c877533123920c9ed8d37d637c03eff73
Naofumi Shiota <2311140037x@ed.fuk.kindai.ac.jp>
2025-12-10 06:26:57 +0000

    ログアウト機能　塩田

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   root "tasks#index"
 
+  get "/top", to: "top#index", as: "top"
+
   resources :users, only: [:new, :create]
 
   # ログイン
   get  '/login',  to: 'sessions#new',     as: 'login'
-  post '/login',  to: 'sessions#create'          
+  post '/login',  to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy', as: 'logout'
 
   resources :tasks do
@@ -14,3 +16,4 @@ Rails.application.routes.draw do
     end
   end
 end
+
